18c0febc0c4b2446deb296de19aae8717a1c920f
komdoroid <kom.commoc@gmail.com>
2024-03-18 22:53:25 +0900

    Django Day 5

--- nippo/views.py
+++ nippo/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def nippoListView(request):
     return render(request, "nippo/nippo-list.html")
+
+def nippoCreateView(request):
+    template_name = "nippo/nippo-form.html"
+
+    if request.POST:
+        title = request.POST["title"]
+        content = request.POST["content"]
+
+    return render(request, template_name)
