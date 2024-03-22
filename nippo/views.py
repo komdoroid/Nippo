52f8294663feb9e12132fac64421bec476274a82
komdoroid <kom.commoc@gmail.com>
2024-03-22 23:37:38 +0900

    Nippo 3-3

--- nippo/views.py
+++ nippo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import NippoModel
+from .forms import NippoFormsClass
 
 # Create your views here.
 def nippoListView(request):
@@ -12,14 +13,21 @@ def nippoListView(request):
 
 def nippoCreateView(request):
     template_name = "nippo/nippo-form.html"
-
-    if request.POST:
-        title = request.POST["title"]
-        content = request.POST["content"]
+    forms = NippoFormsClass(request.POST or None)
+    ctx = {"form": form}
+    if form.is_valid():
+        title = form.cleaned_data["title"]
+        content = form.cleaned_data["content"]
         obj = NippoModel(title=title, content=content)
         obj.save()
 
-    return render(request, template_name)
+    # if request.POST:
+    #     title = request.POST["title"]
+    #     content = request.POST["content"]
+    #     obj = NippoModel(title=title, content=content)
+    #     obj.save()
+
+    return render(request, template_name, ctx)
 
 def nippoDetailView(request, pk):
     template_name = "nippo/nippo-detail.html"
@@ -27,3 +35,17 @@ def nippoDetailView(request, pk):
     q = NippoModel.objects.get(pk=pk)
     ctx["object"] = q
     return render(request, template_name, ctx)
+
+def nippoUpdateFormView(request, pk):
+    template_name = "nippo/nippo-form.html"
+    obj = NippoModel.objects.get(pk=pk)
+    initial_values = {"title": obj.title, "content":obj.content}
+    form = NippoFormsClass(request.POST or initial_values)
+    ctx = {"form": form}
+    if form.is_valid():
+        title = form.cleaned_data["title"]
+        content = form.cleaned_data["content"]
+        obj.title = title
+        obj.content = content
+        obj.save()
+    return render(request, template_name,)
