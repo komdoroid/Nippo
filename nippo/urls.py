4846fcdf1f41d281b5103ba8a60b535d2d449a4c
komdoroid <kom.commoc@gmail.com>
2024-03-27 00:51:32 +0900

    4-1

--- nippo/urls.py
+++ nippo/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("detail/<int:pk>/", nippoDetailView, name="nippo-detail"),
     path("create/", nippoCreateView, name="nippo-create"),
     path("update/<int:pk>/", nippoUpdateFormView, name="nippo-form"),
+    path("update/<int:pk>/", nippoUpdateFormView, name="nippo-update"),
     path("delete/<int:pk>/", nippoDeleteView, name="nippo-delete")
 ]
